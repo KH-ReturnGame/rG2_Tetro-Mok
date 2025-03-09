51ec6c747ebd5eaf3fb3e9b748a8cf1845d58686
sukonbu04299 <choijiwan1206@gmail.com>
2025-03-09 15:57:50 +0900

    changed & add graphic
    Added pixel graphic of stones, ui, and board. Changing BG to pixel graphic remaining

---
+++ Assets/Scripts/GlobalGameData.cs
@@ -0,0 +1,6 @@
+﻿public static class GlobalGameData
+{
+    public static int[,] GameBoard = new int[19, 19];
+    /**흑돌: true, 백돌: false */
+    public static bool IsPlayer1 = true;
+}
--- Assets/Scripts/InitBoard.cs
+++ Assets/Scripts/InitBoard.cs
@@ -6,7 +6,7 @@ public class InitBoard : MonoBehaviour
     [SerializeField] private GameObject side;
     [SerializeField] private GameObject corner;
 
-    GameObject[,] _board = new GameObject[19, 19];
+    private GameObject[,] _board = new GameObject[19, 19];
 
     void Start()
     {
@@ -14,21 +14,30 @@ public class InitBoard : MonoBehaviour
         {
             for (int y = -8; y <= 8; y++)
             {
-                _board[x+8, y+8] = Instantiate(mid, new Vector3(x * 0.5f, y * 0.5f, 0), Quaternion.identity);
+                _board[x+9, y+9] = Instantiate(mid, new Vector3(x * 0.5f, y * 0.5f, 0), Quaternion.identity);
+                _board[x+9, y+9].transform.SetParent(gameObject.transform);
             }
         }
 
         for (int i = -8; i <= 8; i++)
         {
-            _board[i+8, 0] = Instantiate(side, new Vector3(i * 0.5f, 4.5f, 0), Quaternion.Euler(0, 0, 90));
-            _board[i+8, 18] = Instantiate(side, new Vector3(i * 0.5f, -4.5f, 0), Quaternion.Euler(0, 0, -90));
-            _board[0, i+8] = Instantiate(side, new Vector3(-4.5f, i * 0.5f, 0), Quaternion.Euler(0, 0, 180));
-            _board[18, i+8] = Instantiate(side, new Vector3(4.5f, i * 0.5f, 0), Quaternion.Euler(0, 0, 0));
+            _board[i+9, 0] = Instantiate(side, new Vector3(i * 0.5f, 4.5f, 0), Quaternion.Euler(0, 0, -90));
+            _board[i+9, 18] = Instantiate(side, new Vector3(i * 0.5f, -4.5f, 0), Quaternion.Euler(0, 0, 90));
+            _board[0, i+9] = Instantiate(side, new Vector3(-4.5f, i * 0.5f, 0), Quaternion.Euler(0, 0, 0));
+            _board[18, i+9] = Instantiate(side, new Vector3(4.5f, i * 0.5f, 0), Quaternion.Euler(0, 0, 180));
+            _board[i+9, 0].transform.SetParent(gameObject.transform);
+            _board[i+9, 18].transform.SetParent(gameObject.transform);
+            _board[0, i+9].transform.SetParent(gameObject.transform);
+            _board[18, i+9].transform.SetParent(gameObject.transform);
         }
 
-        _board[0,0] = Instantiate(corner, new Vector3(-4.5f, 4.5f, 0), Quaternion.Euler(0, 0, 180));
-        _board[18,0] = Instantiate(corner, new Vector3(4.5f, 4.5f, 0), Quaternion.Euler(0, 0, 90));
-        _board[18,18] = Instantiate(corner, new Vector3(4.5f, -4.5f, 0), Quaternion.identity);
-        _board[0,18] = Instantiate(corner, new Vector3(-4.5f, -4.5f, 0), Quaternion.Euler(0, 0, -90));
+        _board[0,0] = Instantiate(corner, new Vector3(-4.5f, 4.5f, 0), Quaternion.Euler(0, 0, 0));
+        _board[18,0] = Instantiate(corner, new Vector3(4.5f, 4.5f, 0), Quaternion.Euler(0, 0, -90));
+        _board[0,18] = Instantiate(corner, new Vector3(-4.5f, -4.5f, 0), Quaternion.Euler(0, 0, 90));
+        _board[18,18] = Instantiate(corner, new Vector3(4.5f, -4.5f, 0), Quaternion.Euler(0, 0, 180));
+        _board[0, 0].transform.SetParent(gameObject.transform);
+        _board[18, 0].transform.SetParent(gameObject.transform);
+        _board[0, 18].transform.SetParent(gameObject.transform);
+        _board[18, 18].transform.SetParent(gameObject.transform);
     }
 }
