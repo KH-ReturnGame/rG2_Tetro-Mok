43245df2531187fce691e45005f41e81500421fd
sukonbu04299 <choijiwan1206@gmail.com>
2025-04-01 14:27:12 +0900

    starting multiplayer games
    by Unity 6

---
+++ Assets/Scripts/Global/SceneHandler.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Global
+{
+    public class SceneHandler : MonoBehaviour
+    {
+        public static void LoadScene(string sceneName)
+        {
+            SceneManager.LoadScene(sceneName);
+        }
+    }
+}
---
+++ Assets/Scripts/Global/TitleManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Global
+{
+    public class TitleManager : MonoBehaviour
+    {
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Escape)) ExitGame();
+        }
+
+        public void ExitGame()
+        {
+            Application.Quit();
+        }
+    }
+}
