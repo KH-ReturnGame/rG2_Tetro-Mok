d6ee7ebc475f5e9710c1a7de5bcc2a2f487338cb
DNSK <sslgh1024@naver.com>
2025-05-09 22:42:55 +0900

    finish? ai

--- Assets/Scripts/SinglePlay2/GameManager.cs
+++ Assets/Scripts/SinglePlay2/GameManager.cs
@@ -61,7 +61,7 @@ namespace SinglePlay2
         public int count = 0;
         
         [HideInInspector]
-        public double max_count_ = 55;
+        public double max_count_ = 64;
         public bool BlackAI = false;
         public bool WhiteAI = false;
         public MyAgent Black_Agent;
